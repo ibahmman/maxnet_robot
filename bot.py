98a2008b9f3debe5d7f59e857cd6503ce84a21b2
ibahmman <bahmansobhan@gmail.com>
2024-04-15 17:06:09 +0330

    :)

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import requests
 api_url = 'http://127.0.0.1:8000/api/v2/product/v2r/'
 
 async def products(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    response = requests.get(api_url).json()
+    response = requests.get(api_url, headers={'content-type': 'application/json'}).json()
     print(response)
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
